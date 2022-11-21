2a42d00d5d60bfd7fbc3ea06fabd02399e2fb98e
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2022-11-21 12:07:40 +0530

    Readme Added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-console.log(15)
-console.log(10)
+const express = require('express');
+// const route = require("./routes/router")
+const mongoose  = require('mongoose');
+const app = express();
+
+
+app.use(express.json())
+
+
+mongoose.connect("mongodb+srv://izazsarkar11:pQ1xcwJzAI5R7SC6@izazlithium.7ghyokt.mongodb.net/Project-2", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+// app.use( (req ,res) => {
+//     res.status(404).send({status : false , message :`Page Not Found , Given URL ${req.url} is incorrect for this application.`})
+// })
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
