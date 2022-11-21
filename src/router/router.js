d75f31a2fb51b554acc3f2976869708ceea99c2a
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2022-11-21 16:20:21 +0530

    All Done

--- src/router/router.js
+++ src/router/router.js
@@ -8,11 +8,6 @@ const collegeController = require('../controller/collegeController')
 router.get("/test-me", function (req, res) { res.status(200).send("Connection Done.") })
 
 
-// // // Model improt are -->
-const collegeModel = require("../model/collegeModel")
-const internModel = require("../model/internModel")
-
-
 // // // ### POST /functionup/interns
 router.post("/functionup/interns" , internController.createIntern)
 
