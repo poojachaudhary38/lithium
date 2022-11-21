783de01d02974d20b7d9941843414c267493c19c
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2022-11-21 12:12:16 +0530

    File done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const route = require("./routes/router")
+const route = require("./routes/router")
 const mongoose  = require('mongoose');
 const app = express();
 
